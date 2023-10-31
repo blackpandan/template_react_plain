0006abf2c3fae0277e02bcf14a918c47a8cdd378
devfate <faithgstc54@gmail.com>
2023-10-31 05:03:47 +0100

    TEMPLATE/EDIT: added toastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ROUTES from "./constants/ROUTES.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const router = createBrowserRouter(ROUTES);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
